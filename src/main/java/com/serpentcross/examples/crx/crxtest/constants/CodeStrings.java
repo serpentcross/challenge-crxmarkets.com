26207f7ebc783bbcbe824bb5210c3ba9d113b7b9
Vladimir <serpentcross@mail.ru>
2017-04-28 02:23:27 +0300

    Added. Main part of the task.

---
+++ src/main/java/com/serpentcross/examples/crx/crxtest/constants/CodeStrings.java
@@ -0,0 +1,7 @@
+package com.serpentcross.examples.crx.crxtest.constants;
+
+public class CodeStrings {
+
+    public static final String ANSWER = "here will be your answer";
+    public static final String ERROR_TEXT = "Input is incorrect: ";
+}
---
+++ src/main/java/com/serpentcross/examples/crx/crxtest/views/MainView.java
@@ -0,0 +1,94 @@
+package com.serpentcross.examples.crx.crxtest.views;
+
+import com.serpentcross.examples.crx.crxtest.constants.CodeStrings;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import java.util.Random;
+
+@ManagedBean(name = "mainView")
+@ViewScoped
+public class MainView {
+
+    @Getter @Setter private String textValue;
+    @Getter @Setter private String[] initialArray;
+
+    private int[] intArray;
+
+    @Getter @Setter private String answer = null;
+    @Getter @Setter private boolean submitIsDisabled = true;
+
+    public void renderInitialArray() {
+
+        int arraySize;
+
+        try {
+
+            answer = CodeStrings.ANSWER;
+
+            Random random = new Random();
+            arraySize = Integer.parseInt(textValue);
+
+            initialArray = new String[arraySize];
+            intArray = new int[arraySize];
+
+            if(arraySize < 0) {
+                arraySize = 0;
+                initialArray = null;
+            }
+
+            for(int i = 0; i < arraySize; i++) {
+                initialArray[i] = Integer.toString(random.nextInt(arraySize) + 1);
+                intArray[i] = Integer.parseInt(initialArray[i]);
+            }
+
+            submitIsDisabled = false;
+
+        } catch (NumberFormatException ex) {
+            submitIsDisabled = true;
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", CodeStrings.ERROR_TEXT + textValue);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+
+    public void submitArrayAction(ActionEvent actionEvent) {
+        answer = Integer.toString(calculate(getIntArray()));
+    }
+
+    private static int calculate(int[] src) {
+
+        int volume = 0;
+
+        // Precalculate the maximum landscape height to the right from the current position
+        int rightMaxLevel[] = new int[src.length];
+        
+        for (int i = src.length - 1; i > 0 ; --i) {
+            rightMaxLevel[i - 1] = Math.max(src[i], rightMaxLevel[i]);
+        }
+
+        int leftMaxLevel = 0;
+
+        for (int i = 0; i < src.length; ++i) {
+            // Get the maximum water height at the given position
+            int maxLevel = Math.min(leftMaxLevel, rightMaxLevel[i]);
+
+            // Increase the volume by the water height in the given position
+            volume += Math.max(maxLevel - src[i], 0);
+
+            // Calculate the left maximum level
+            leftMaxLevel = Math.max(leftMaxLevel, src[i]);
+        }
+
+        return volume;
+    }
+
+    private int[] getIntArray() {
+        return intArray;
+    }
+}
